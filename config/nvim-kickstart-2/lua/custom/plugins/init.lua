bbba3f07eeaadab4fb7cac10faab7aa31007d3fe
brightkut <dsorn2@gmail.com>
2025-05-26 06:46:34 +0700

    add new kickstart update version

---
+++ config/nvim-kickstart-2/lua/custom/plugins/init.lua
@@ -0,0 +1,147 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+  -- NOTE: add this plugin if you want to have nice UI message box
+  -- {
+  --   'folke/noice.nvim',
+  --   event = 'VeryLazy',
+  --   opts = {
+  --     -- add any options here
+  --   },
+  --   dependencies = {
+  --     -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+  --     'MunifTanjim/nui.nvim',
+  --     -- OPTIONAL:
+  --     --   `nvim-notify` is only needed, if you want to use the notification view.
+  --     --   If not available, we use `mini` as the fallback
+  --     'rcarriga/nvim-notify',
+  --   },
+  -- },
+  { 'folke/snacks.nvim', opts = { dashboard = { enabled = false } } },
+  {
+    'nvimdev/dashboard-nvim',
+    lazy = false, -- As https://github.com/nvimdev/dashboard-nvim/pull/450, dashboard-nvim shouldn't be lazy-loaded to properly handle stdin.
+    opts = function()
+      local logo = [[
+██╗      ██████╗ ███╗   ██╗███████╗██████╗ 
+██║     ██╔═══██╗████╗  ██║██╔════╝██╔══██╗
+██║     ██║   ██║██╔██╗ ██║█████╗  ██████╔╝
+██║     ██║   ██║██║╚██╗██║██╔══╝  ██╔══██╗
+███████╗╚██████╔╝██║ ╚████║███████╗██║  ██║
+╚══════╝ ╚═════╝ ╚═╝  ╚═══╝╚══════╝╚═╝  ╚═╝
+                                           
+    ]]
+
+      logo = string.rep('\n', 8) .. logo .. '\n\n'
+
+      local opts = {
+        theme = 'doom',
+        hide = {
+          -- this is taken care of by lualine
+          -- enabling this messes up the actual laststatus setting after loading a file
+          statusline = false,
+        },
+        config = {
+          header = vim.split(logo, '\n'),
+        -- stylua: ignore
+        center = {
+          { action = 'lua LazyVim.pick()()',                           desc = " Find File",       icon = " ", key = "f" },
+          { action = "ene | startinsert",                              desc = " New File",        icon = " ", key = "n" },
+          { action = 'lua LazyVim.pick("oldfiles")()',                 desc = " Recent Files",    icon = " ", key = "r" },
+          { action = 'lua LazyVim.pick("live_grep")()',                desc = " Find Text",       icon = " ", key = "g" },
+          { action = 'lua LazyVim.pick.config_files()()',              desc = " Config",          icon = " ", key = "c" },
+          { action = 'lua require("persistence").load()',              desc = " Restore Session", icon = " ", key = "s" },
+          { action = "LazyExtras",                                     desc = " Lazy Extras",     icon = " ", key = "x" },
+          { action = "Lazy",                                           desc = " Lazy",            icon = "󰒲 ", key = "l" },
+          { action = function() vim.api.nvim_input("<cmd>qa<cr>") end, desc = " Quit",            icon = " ", key = "q" },
+        },
+          footer = function()
+            local stats = require('lazy').stats()
+            local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+            return { '⚡ Neovim loaded ' .. stats.loaded .. '/' .. stats.count .. ' plugins in ' .. ms .. 'ms' }
+          end,
+        },
+      }
+
+      for _, button in ipairs(opts.config.center) do
+        button.desc = button.desc .. string.rep(' ', 43 - #button.desc)
+        button.key_format = '  %s'
+      end
+
+      -- open dashboard after closing lazy
+      if vim.o.filetype == 'lazy' then
+        vim.api.nvim_create_autocmd('WinClosed', {
+          pattern = tostring(vim.api.nvim_get_current_win()),
+          once = true,
+          callback = function()
+            vim.schedule(function()
+              vim.api.nvim_exec_autocmds('UIEnter', { group = 'dashboard' })
+            end)
+          end,
+        })
+      end
+
+      return opts
+    end,
+  },
+  -- NOTE: install web devicons for bufferline
+  { 'nvim-tree/nvim-web-devicons', opts = {} },
+  -- NOTE: Plugin for setup tab UI
+  {
+    'akinsho/bufferline.nvim',
+    version = '*',
+    dependencies = 'nvim-tree/nvim-web-devicons',
+    config = function()
+      local bufferline = require 'bufferline'
+      bufferline.setup {
+        options = {
+          style_preset = {
+            bufferline.style_preset.no_italic,
+            bufferline.style_preset.no_bold,
+          },
+          hover = {
+            enabled = true,
+            delay = 200,
+            reveal = { 'close' },
+          },
+          show_buffer_icons = true, -- show icon file type
+          -- other options here
+          separator_style = 'thin',
+          diagnostics = 'nvim_lsp',
+          show_close_icon = false,
+          diagnostics_indicator = function(count, level, diagnostics_dict, context)
+            local s = ' '
+            for e, n in pairs(diagnostics_dict) do
+              local sym = e == 'error' and ' ' or (e == 'warning' and ' ' or ' ')
+              s = s .. n .. sym
+            end
+            return s
+          end,
+          offsets = {
+            {
+              filetype = 'neo-tree',
+              text = 'Neo-tree',
+              highlight = 'Directory',
+              text_align = 'left',
+            },
+            {
+              filetype = 'snacks_layout_box',
+            },
+          },
+        },
+      }
+    end,
+  },
+  {
+    'rest-nvim/rest.nvim',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+      opts = function(_, opts)
+        opts.ensure_installed = opts.ensure_installed or {}
+        table.insert(opts.ensure_installed, 'http')
+      end,
+    },
+  },
+}
