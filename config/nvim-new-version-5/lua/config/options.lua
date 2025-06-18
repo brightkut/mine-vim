6c2862d076cd8e712dea07cf4dddbb0ea3951241
LonEr <dsorn2@gmail.com>
2025-06-18 23:53:51 +0700

    feat: add new config nvim

---
+++ config/nvim-new-version-5/lua/config/options.lua
@@ -0,0 +1,10 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+
+--- Disable Swap file
+vim.opt.swapfile = false
+vim.opt.backup = false
+-- vim.opt.smoothscroll = true
+
+-- vim.opt.foldmethod = "expr"
+-- vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
